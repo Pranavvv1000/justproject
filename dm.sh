4f28ba8ebd80904e44816d6ebae5933106ec25e3
Pranavvv1000 <132485339+Pranavvv1000@users.noreply.github.com>
2023-05-05 22:23:30 +0530

    Update dm.sh

--- dm.sh
+++ dm.sh
@@ -3,7 +3,7 @@
 python -m pip install --upgrade pip
 pip3 install flask
 pip3 install --upgrade Flask
-pip install datetime
+pip3 install datetime
 pip3 install yfinance
 pip3 install Figure
 pip3 install matplotlib
